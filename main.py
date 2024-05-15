146233643a6886dece33177302d1255e495cac65
brillioccai <riturajbrillio@gmail.com>
2024-05-15 23:21:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src import *
 
-conv_id, ha_id, eu_id = create_conversation_pipeline.create_pipeline("brldi-ds-capabilities-ccai", "FAQ_KB_NEW_3", "FAQ_ASSIST_NEW_3", "CONV_PROFILE_NEW_3")
+conv_id, ha_id, eu_id = create_conversation_pipeline.create_pipeline(project_id, "FAQ_KB_NEW_3", "FAQ_ASSIST_NEW_3", "CONV_PROFILE_NEW_3")
 
-conversation_on-off.create_conversation_pipeline("brldi-ds-capabilities-ccai", conv_id, ha_id, eu_id, "My credit card payment is declined", "")
+conversation_on-off.create_conversation_pipeline(project_id, conv_id, ha_id, eu_id, "My credit card payment is declined", "")
